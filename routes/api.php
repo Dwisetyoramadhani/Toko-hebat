58b98cd7393b424be9020606d188bb566e846117
Dwisetyoramadhani <dsrtyo11@gmail.com>
2026-05-08 15:41:53 +0700

    fix

--- routes/api.php
+++ routes/api.php
@@ -8,6 +8,16 @@ use App\Http\Controllers\ProductController;
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
-Route::apiResource('categories', CategoryController::class)->middleware('auth:sanctum');
 
-Route::apiResource('products', ProductController::class)->middleware('auth:sanctum');
+Route::middleware('auth:sanctum')->group(function () {
+    Route::apiResource('categories', CategoryController::class)->only(['index', 'show']);
+});
+Route::middleware('auth:sanctum', 'admin')->group(function () {
+    Route::apiResource('categories', CategoryController::class)->except(['index', 'show']);
+});
+Route::middleware('auth:sanctum')->group(function () {
+    Route::apiResource('products', ProductController::class)->only(['index', 'show']);
+});;
+Route::middleware('auth:sanctum', 'admin')->group(function () {
+    Route::apiResource('products', ProductController::class)->except(['index', 'show']);
+});
